b7a6aea30f54e1efa0dad3bb88786eb13439af51
Josh-DEV15 <risingsun15052005@gmail.com>
2026-04-10 11:43:55 +0100

    Second commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,26 @@ import Contacts from './components/Contacts.jsx';
 import Projects from './components/Projects.jsx';
 import Skills from './components/Skills.jsx';
 import Home from './components/Home.jsx';
-function App(){
-    return(
+import WhatsappFloat from './components/WhatsappFloat.jsx';
+import HireMeFloat from './components/HireMeFloat.jsx';
+import Navbar from './components/Navbar.jsx';
+import Services from './components/Services.jsx';
+import TestimonialSection from './components/TestimonialSection.jsx';
+function App() {
+    return (
         <div className="App">
             <h1>Welcome to my portfolio</h1>
+            <Navbar />
             <Home />
+            <WhatsappFloat />
             <About />
             <Projects />
+            <Services />
             <Skills />
+            <TestimonialSection />
             <Contacts />
             <Footer />
+            <HireMeFloat />
         </div>
     );
 }
